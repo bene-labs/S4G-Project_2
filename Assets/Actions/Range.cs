2db98676db35a1a033c5cd80517558fd6e6da743
Suchti0352 <136480895+Suchti0352@users.noreply.github.com>
2023-06-20 17:46:30 +0200

    Add Action System Template

---
+++ Assets/Actions/Range.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RangeAction", menuName = "Actions/Range Attack", order = 1)]
+public class Range : Attack
+{
+    [Header("Range")]
+    [SerializeField]
+    private GameObject projectile;
+
+    public override void Perform()
+    {
+        base.Perform();
+    }
+}
